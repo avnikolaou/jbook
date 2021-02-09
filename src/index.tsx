f2a8c55f11e36cc2dcf8eab0d6a586129eaa371d
avnik <avnikolaou@outlook.com>
2021-02-09 12:35:24 +0200

    feat: safe handling untrusted code execution

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
     const ref = useRef<any>();
+    const iframe = useRef<any>();
     const [input, setInput] = useState('');
-    const [code, setCode] = useState('');
 
     const startService = async () => {
         ref.current = await esbuild.startService({
@@ -25,6 +25,8 @@ const App = () => {
             return
         }
 
+        iframe.current.srcdoc = html;
+
         const result = await ref.current.build({
             entryPoints: ['index.js'],
             bundle: true,
@@ -39,16 +41,41 @@ const App = () => {
             }
         });
 
-        setCode(result.outputFiles[0].text)
+        // setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
     };
 
-    return <div>
-        <textarea value={input} onChange={e => setInput(e.target.value)}/>
+    const html = `
+            <html>
+                <head>
+                </head>
+                <body>
+                    <div id="root">
+                        <script>
+                        window.addEventListener('message', (e) => {
+                            try {
+                                eval(e.data);
+                            } catch (err) {
+                                const root = document.querySelector('#root');
+                                root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+                                console.error(err);
+                            }
+                        }, false)
+                        </script>
+                    </div>
+                </body>
+            </html>
+    `;
+
+    return (
         <div>
-            <button onClick={onClick}>Submit</button>
+            <textarea value={input} onChange={e => setInput(e.target.value)}/>
+            <div>
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <iframe title={'preview'} ref={iframe} sandbox={"allow-scripts"} srcDoc={html} />
         </div>
-        <pre>{code}</pre>
-    </div>
-}
+    );
+};
 
 ReactDOM.render(<App />, document.querySelector('#root'));
