d13060eed7fa5350b5583d3bd9f21876cb391388
AvraamNikolaou <avnikolaou@hotmail.com>
2021-02-10 12:13:45 +0200

    feat: draggable resizing components

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -60,7 +60,7 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialVaule}) => {
                 value={initialVaule}
                 theme={'dark'}
                 language={'javascript'}
-                height={'500px'}
+                height={'100%'}
                 options={{
                     wordWrap: 'on',
                     minimap: { enabled: false },
--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from 'react';
+import './preview.css'
 
 interface PreviewProps {
     code: string;
@@ -7,6 +8,7 @@ interface PreviewProps {
 const html = `
         <html>
             <head>
+                <style> html { background-color: white }</style>
             </head>
             <body>
                 <div id='root'>
@@ -31,10 +33,14 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
 
     useEffect(() => {
         iframe.current.srcdoc = html;
-        iframe.current.contentWindow.postMessage(code, '*');
+        setTimeout(() => {
+            iframe.current.contentWindow.postMessage(code, '*');
+        }, 50)
     }, [code]);
 
-    return <iframe title={'preview'} ref={iframe} sandbox={'allow-scripts'} srcDoc={html} />
+    return <div className={'preview-wrapper'}>
+        <iframe title={'preview'} ref={iframe} sandbox={'allow-scripts'} srcDoc={html} />
+    </div>
 };
 
 export default Preview;
