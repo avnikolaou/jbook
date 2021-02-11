5f60c141efa7630767c3b698be654f1dde8cfb04
AvraamNikolaou <avnikolaou@hotmail.com>
2021-02-11 20:24:21 +0200

    feat: connecting bundles in redux

--- src/state/action-types/index.ts
+++ src/state/action-types/index.ts
@@ -2,5 +2,7 @@ export enum ActionType {
     MOVE_CEELL = 'move_cell',
     DELETE_CELL = 'delete_cell',
     INSERT_CELL_AFTER = 'insert_cell_after',
-    UPDATE_CELL = 'update_cell'
+    UPDATE_CELL = 'update_cell',
+    BUNDLE_START = 'bandle_start',
+    BUNDLE_COMPLETE = 'bandle_complete'
 }
